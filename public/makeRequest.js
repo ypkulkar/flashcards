c55be5d17d19dae7ed95d7da7a65b2b7bf735dca
Kirsten <kegillam@ucdavis.edu>
2019-05-16 00:38:58 +0000

    makeRequest

---
+++ public/makeRequest.js
@@ -0,0 +1,42 @@
+"strict mode";
+
+// Do a CORS request to get Davis weather hourly forecast
+
+// Create the XHR object.
+function createRequest(method, url) {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);  // call its open method
+  return xhr;
+}
+
+// Make the actual CORS request.
+function makeRequest(word) {
+
+  let url = `query?word=${word}`;
+
+  let xhr = createRequest('GET', url);
+
+  // checking if browser does CORS
+  if (!xhr) {
+    alert('error making request');
+    return;
+  }
+
+  // Load some functions into response handlers.
+  xhr.onload = function() {
+      //console.log(xhr);
+      let responseStr = xhr.response;  // get the JSON string 
+      let object = JSON.parse(responseStr);  // turn it into an object
+      //console.log(JSON.stringify(object, undefined, 2));  // print it out as a string, nicely formatted
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  // Actually send request to server
+  xhr.send();
+
+  return xhr;
+}
+
