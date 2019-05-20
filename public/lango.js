14490c201865e78c67a8434f60dcbed5a28a7f66
Kirsten <kegillam@ucdavis.edu>
2019-05-19 21:59:03 -0700

    adding old version of file cause we lost the recent one :'(

---
+++ public/lango.js
@@ -0,0 +1,25 @@
+const button = document.querySelector("#submit-button");
+const input = document.querySelector("#phrase");
+const output = document.querySelector("#translation");
+
+button.addEventListener("click",function(){
+	var phrase = input.value;
+	xhr = makeRequest(phrase);
+	xhr.onreadystatechange = orscFunction;
+});
+
+orscFunction = function(){
+	console.log("Ready state change");
+	if(xhr.readyState == 4){
+		if(xhr.status == 200){
+			object = JSON.parse(xhr.responseText);
+			output.textContent = object.translation;
+			console.log(object.translation);	
+		}
+		
+		if(xhr.status == 404){
+			console.log("Error 404: your life is fucked")
+		}
+	}
+
+};
